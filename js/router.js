b90b8ae8026f26b672436d6b156d6be897a8a527
Trek Glowacki <trek.glowacki@gmail.com>
2013-03-14 22:40:13 -0500

    Transitioning to show only complete

--- js/router.js
+++ js/router.js
@@ -2,6 +2,7 @@ Todos.Router.map(function () {
   this.resource('todos', { path: '/' }, function () {
     // additional child routes    
     this.route('active');
+    this.route('completed');
   });
 });
 
@@ -21,6 +22,17 @@ Todos.TodosActiveRoute = Ember.Route.extend({
   model: function(){
     return this.store.filter('todo', function (todo) {
       return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller){
+    this.render('todos/index', {controller: controller});
+  }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function (todo) {
+      return todo.get('isCompleted');
     });
   },
   renderTemplate: function(controller){
