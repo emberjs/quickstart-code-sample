81801d87da42d0c83685ff946c46de68589ce38f
Trek Glowacki <trek.glowacki@gmail.com>
2013-03-14 23:07:50 -0500

    Replacing the fixture store with another adapter

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 window.Todos = Ember.Application.create();
 
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+  namespace: 'todos-emberjs'
+});
