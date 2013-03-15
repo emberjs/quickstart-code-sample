ddcb1b480837144c4051d098c476811de81beded
Trek Glowacki <trek.glowacki@gmail.com>
2013-03-14 22:30:54 -0500

    Transitioning between routes

--- js/router.js
+++ js/router.js
@@ -1,8 +1,16 @@
 Todos.Router.map(function () {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function () {
+    // additional child routes    
+  });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
+  model: function () {
+    return Todos.Todo.find();
+  }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
   model: function () {
     return Todos.Todo.find();
   }
