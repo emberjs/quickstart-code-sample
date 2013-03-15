44e76869476691787957c0ef919b35cbd3f7d0f4
Trek Glowacki <trek.glowacki@gmail.com>
2013-03-14 21:30:47 -0500

    Marking a Model as Complete or Incomplete

---
+++ js/controllers/todo_controller.js
@@ -0,0 +1,15 @@
+Todos.TodoController = Ember.ObjectController.extend({
+  isCompleted: function(key, value){
+    var model = this.get('model');
+
+    if (value === undefined) {
+      // property being used as a getter
+      return model.get('isCompleted');
+    } else {
+      // property being used as  setter
+      model.set('isCompleted', value);
+      model.save();
+      return value;
+    }
+  }.property('model.isCompleted')
+});
