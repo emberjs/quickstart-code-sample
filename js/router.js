72b1ccde5e157b20fcfe5da9bd52496e73533d47
Trek Glowacki <trek.glowacki@gmail.com>
2013-03-14 20:43:32 -0500

    Displaying Model Data

--- js/router.js
+++ js/router.js
@@ -1,3 +1,9 @@
 Todos.Router.map(function () {
   this.resource('todos', { path: '/' });
 });
+
+Todos.TodosRoute = Ember.Route.extend({
+  model: function () {
+    return Todos.Todo.find();
+  }
+});
