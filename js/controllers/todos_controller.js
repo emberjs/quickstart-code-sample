d2763925d43e196cd167e018d9d1c05fd7f80cf1
Trek Glowacki <trek.glowacki@gmail.com>
2013-03-14 22:48:39 -0500

    Displaying a button to remove all completed todos

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -16,6 +16,11 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       // Save the new model
       todo.save();
+    },
+    clearCompleted: function () {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   },
 
@@ -26,5 +31,13 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function () {
     var remaining = this.get('remaining');
     return remaining === 1 ? 'item' : 'items';
-  }.property('remaining')
+  }.property('remaining'),
+
+  hasCompleted: function () {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function () {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted')
 });
