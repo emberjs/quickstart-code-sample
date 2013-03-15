4830fbe41ed41326ac26025cb98104a0c258dd03
Trek Glowacki <trek.glowacki@gmail.com>
2013-03-14 23:07:50 -0500

    Replacing the fixture store with another adapter

--- js/models/store.js
+++ js/models/store.js
@@ -1,4 +1,8 @@
 Todos.Store = DS.Store.extend({
   revision: 12,
-  adapter: 'DS.FixtureAdapter'
+  adapter: 'Todos.LSAdapter'
+});
+
+Todos.LSAdapter = DS.LSAdapter.extend({
+  namespace: 'todos-emberjs'
 });
