2a1d35293a52e40d0125f552a1a8b2c01f759313
Trek Glowacki <trek.glowacki@gmail.com>
2013-03-14 22:36:37 -0500

    Transitioning to show only incomplete todos

--- js/router.js
+++ js/router.js
@@ -1,6 +1,7 @@
 Todos.Router.map(function () {
   this.resource('todos', { path: '/' }, function () {
     // additional child routes    
+    this.route('active');
   });
 });
 
@@ -13,5 +14,16 @@ Todos.TodosRoute = Ember.Route.extend({
 Todos.TodosIndexRoute = Ember.Route.extend({
   model: function () {
     return this.modelFor('todos');
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function (todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller){
+    this.render('todos/index', {controller: controller});
   }
 });
