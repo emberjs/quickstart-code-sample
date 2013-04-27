trek/todomvc-as-tutorial

Displaying a button to remove all completed todos

js/controllers/todos_controller.js
@@ -24,5 +24,20 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function () {
     var remaining = this.get('remaining');
     return remaining === 1 ? 'item' : 'items';
-  }.property('remaining')
+  }.property('remaining'),
+
+  hasCompleted: function () {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function () {
+    return this.filterProperty('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+  
+  clearCompleted: function () {
+    var completed = this.filterProperty('isCompleted', true);
+    completed.invoke('deleteRecord');
+
+    this.get('store').commit();
+  }
 });

emberjs/quickstart-code-sample

js/controllers/todos_controller.js
@@ -24,5 +24,20 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function () {
     var remaining = this.get('remaining');
     return remaining === 1 ? 'item' : 'items';
-  }.property('remaining')
+  }.property('remaining'),
+
+  hasCompleted: function () {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function () {
+    return this.filterProperty('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+  
+  clearCompleted: function () {
+    var completed = this.filterProperty('isCompleted', true);
+    completed.invoke('deleteRecord');
+
+    this.get('store').save();
+  }
 });