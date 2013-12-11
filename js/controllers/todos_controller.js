trek/todomvc-as-tutorial

Displaying the number of incomplete todos

js/controllers/todos_controller.js
@@ -15,5 +15,14 @@ Todos.TodosController = Ember.ArrayController.extend({
 
     // Save the new model
     this.get('store').commit();
-  }
+  },
+
+  remaining: function () {
+    return this.filterProperty('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function () {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'item' : 'items';
+  }.property('remaining')
 });

emberjs/quickstart-code-sample

js/controllers/todos_controller.js
@@ -17,5 +17,14 @@ Todos.TodosController = Ember.ArrayController.extend({
       // Save the new model
       todo.save();
     }
-  }
+  },
+
+  remaining: function () {
+    return this.filterBy('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function () {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'item' : 'items';
+  }.property('remaining')
 });