24aee61a713df2cedf903256f9621924aaf6b81c
Keri Clowes <Kerilyn.Clowes@gmail.com>
2014-11-22 12:46:06 -0700

    Creating a New Model Instance

---
+++ js/controllers/todos_controller.js
@@ -0,0 +1,22 @@
+Todos.TodosController = Ember.ArrayController.extend({
+  actions: {
+    createTodo: function () {
+      // Get the todo title set by the "New Todo" text field
+      var title = this.get('newTitle');
+      if (!title) { return false; }
+      if (!title.trim()) { return; }
+
+      // Create the new Todo model
+      var todo = this.store.createRecord('todo', {
+        title: title,
+        isCompleted: false
+      });
+
+      // Clear the "New Todo" text field
+      this.set('newTitle', '');
+
+      // Save the new model
+      todo.save();
+    }
+  }
+});
